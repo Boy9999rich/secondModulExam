b10e431197096aff866b2bb24e93f87b137179f2
Boy9999rich <boymurodmamarajapov1@gmail.com>
2025-01-07 09:21:32 +0500

    for exam

---
+++ 01__072__2025__2__modul__Exam/01__072__2025__2__modul__Exam/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _01__072__2025__2__modul__Exam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
